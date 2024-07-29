ba9ab96d2b505845ed1171ecebf7e9f00207de3d
rbgwangwa <rbgwangwa@gmail.com>
2024-07-29 20:49:30 +0200

    initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/raesibegwangwa/sort_thee_waste/SortTheeWasteApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.raesibegwangwa.sort_thee_waste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortTheeWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortTheeWasteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/raesibegwangwa/sort_thee_waste/SortTheeWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.raesibegwangwa.sort_thee_waste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortTheeWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
